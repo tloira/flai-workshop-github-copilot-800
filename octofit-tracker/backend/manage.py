f5d8b578a4d3f15d4185abc3974c5710dd330b0b
tloira <108075766+tloira@users.noreply.github.com>
2026-02-19 16:55:59 +0000

    teste

--- octofit-tracker/backend/manage.py
+++ octofit-tracker/backend/manage.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-#teste 2
-
+#teste234
 
 def main():
     """Run administrative tasks."""
